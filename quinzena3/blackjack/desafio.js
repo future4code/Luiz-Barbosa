054c250b341630db10854e69ccf233f752b7e1ef
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2020-11-29 15:59:41 -0300

    Tentativa do desafio1

--- quinzena3/blackjack/desafio.js
+++ quinzena3/blackjack/desafio.js
@@ -11,3 +11,52 @@
  * 
  */
 
+
+// Essas sao as cartas e pontuações do jogo
+const primeiraUsuario = comprarCarta();
+const segundaUsuario = comprarCarta();
+const pontosUsuario = primeiraUsuario.valor + segundaUsuario.valor;
+const primeiraComputador = comprarCarta();
+const segundaComputador = comprarCarta();
+const pontosComputador = primeiraComputador.valor + segundaComputador.valor;
+//Aqui o jogo inicia.
+if(confirm("Quer iniciar uma nova rodada?")) {
+	console.log("Bem vindo ao jogo de Blackjack!");
+} else {
+   console.log("O jogo acabou.")
+}
+
+// Se as primeiras cartas forem A, sortear de novo
+// if (primeiraComputador || primeiraUsuario === "A") {
+//    comprarCarta();
+// } 
+
+//Aqui o jogo pergunta se deve sortear mais cartas.
+
+if (confirm("Suas cartas são " + primeiraUsuario.texto + "e " + segundaUsuario.texto + " A carta revelada do computador é " + primeiraComputador.texto + " Deseja comprar mais uma carta?")) {
+      comprarCarta();
+   } else {
+      console.log("O jogo acabou!")
+   }
+
+
+console.log("Usuário - cartas: " + primeiraUsuario.texto, segundaUsuario.texto + "- pontuação " + pontosUsuario);
+console.log("Computador - cartas: " + primeiraComputador.texto, segundaComputador.texto + "- pontuação " + pontosComputador) 
+
+const cartasExtraUsuario = [];
+
+
+
+for (confirm("Quer iniciar uma rodada?"); confirm("Deseja outra carta?"); comprarCarta()) {
+   cartasExtraUsuario.push()
+   console.log(cartasExtraUsuario.texto)
+}
+ 
+
+if (pontosUsuario > pontosComputador) {
+    console.log("O usuário ganhou!")
+ } else if (pontosComputador < pontosComputador) {
+  console.log("O computador venceu!")
+   } else {
+      console.log("Empatou!")
+   }
--- quinzena3/blackjack/nossoJogo.js
+++ quinzena3/blackjack/nossoJogo.js
@@ -1,12 +1,41 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+	console.log("Bem vindo ao jogo de Blackjack!");
+} else {
+   console.log("O jogo acabou.")
+}
+
+const primeiraUsuario = comprarCarta();
+const segundaUsuario = comprarCarta();
+const pontosUsuario = primeiraUsuario.valor + segundaUsuario.valor;
+const primeiraComputador = comprarCarta();
+const segundaComputador = comprarCarta();
+const pontosComputador = primeiraComputador.valor + segundaComputador.valor;
+
+
+console.log("Usuário - cartas: " + primeiraUsuario.texto, segundaUsuario.texto + "- pontuação " + pontosUsuario);
+console.log("Computador - cartas: " + primeiraComputador.texto, segundaComputador.texto + "- pontuação " + pontosComputador) 
+
+
+if (pontosUsuario > pontosComputador) {
+   console.log("O usuário ganhou!")
+} else if (pontosComputador < pontosComputador) {
+   console.log("O computador venceu!")
+   } else if (pontosComputador === pontosUsuario) {
+      console.log("Empatou")
+   } else(" {
+      console.log("O jogo acabou")
+   }
+// console.log(primeiraUsuario.texto) 
+// console.log(primeiraUsuario.valor) 
+
+
+//Agora preciso fazer com que o console mostre as seguintes mensagens: "Usuário - cartas: Q♣️ 10♣️  - pontuação 20" 
+//"Computador - cartas: Q♣️ 10♣️  - pontuação 20" 
+//preciso também que o console mostre cartas diferentes toda vez que o jogo iniciar. Preciso criar uma forma de que o console some a pontuação do usuário e do computador. Para que com essa soma, ele decida se o vencedor é o computador ou o usuário e assim monstre a mensagem de quem ganhou. (IF)
+// comprarCarta();
+// const pontosUsuario = cartas.valor;
+// if ()
+
+
+// const pontosComputador = comprarCarta();
