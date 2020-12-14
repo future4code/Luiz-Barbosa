701e377c6794442b76c6563ae983f35ed624334b
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2020-12-14 16:53:49 -0300

    "exercicio 1"

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,13 @@
+//- Exercícios de interpretação de código
+
+/*
+1 - Na primeira função, foi criada uma função que pede ao usuário pra inserir a cotação do dólar, retorna o valor dado como parâmetro na hora de chamar a função, multiplicado pelo valor inserido pelo usuário. Nesse caso, multiplica 100 pelo valor da cotação.
+
+2 - Essa função recebe 2 parâmetros, o primeiro é o tipo de investimento e o segundo o valor em número. A função cria uma variável vazia e seu valor depende do resultado quando o programa entra no switch case logo abaixo. Dependendo do valor inserido na hora de chamar a função, ele entra em um case do switch, calcula e retorna o valor no console log das novas variáveis criadas pra armazenar o resultado. Caso o valor não se encaixe em nenhum dos cases, um alert é mostrado com uma mensagem dizendo que o conteudo inserido é incorreto.
+
+3 -Acredito que a intenção desse loop é descobrir quantos numeros pares e ímpares existem dentro desse primeiro array numeros. Esse loop + if checam se o resto da divisão por 2 dos números é igual a zero, e se sim, joga ele dentro de um array vazio, caso contrário, dentro do segundo array vazio. 
+Depois disso, ele imprime no console.log a quantidade de números dentro dos novos arrays. 
+
+4 - Foi criado um array com vários números e duas variaveis com valor infinity e 0.  Foi criado um for que checa cada item do array pra ver se eles são maiores ou menores que os valores das variáveis numero1 e numero2. 
+Mas pelo que eu entendi, o console vai mostrar os mesmos números no primeiro log. Porque todos os números são menores que infinity. Já no segundo console.log só vai faltar o número negativo, mas vai mostrar todos os outros também.
+*/
