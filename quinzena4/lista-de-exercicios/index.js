b3f0c169a6c98db428d9dbb06155c7800d3001dc
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-01-09 14:18:35 -0300

    funçoes de callback finalizadas, não fiz o ultimo

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -267,14 +267,89 @@ b) Faça uma função que retorne um novo array só com as crianças/adolescente
 
 //c) Escreva uma função que retorne um array de strings dizendo: "${número} é par/impar". Isto é: ["1 é impar", "2 é par", "3 é impar", "4 é par", "5 é impar", "6 é par"] 
 
-const arrayDeNumeros = [1, 2, 3, 4, 5, 6]
-let parouImpar = arrayDeNumeros.map((item, index, array) => {
-    if(item % 2 === 0) {
-            return `${item} é par`
-        } else {
-            return `${item} é ímpar`
-        }
-})
+// const arrayDeNumeros = [1, 2, 3, 4, 5, 6]
+// let parouImpar = arrayDeNumeros.map((item, index, array) => {
+//     if(item % 2 === 0) {
+//             return `${item} é par`
+//         } else {
+//             return `${item} é ímpar`
+//         }
+// })
 
-console.log(parouImpar)
+// console.log(parouImpar)
 
+//2 - Imagine que você trabalhe num parque de diversões, como desenvolvedor(a). As suas tarefas são sempre com o intuito de ajudar a automação de alguns processos internos do parque. Uma das atrações principais dele é a montanha russa do terror. As filas são muito grandes e todas as pessoas de várias idades insistem entrar no brinquedo, mesmo sabendo que não podem. Considere, então, essas pessoas:
+
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+/*A regra para entrar na montanha russa do terror é: ter, no mínimo, 1.5m de altura; ser mais velho do que 14 anos e mais novo do que 60 anos.
+
+a) Escreva uma **função** que **receba** este array e **devolva** outro array somente com as pessoas que **tem permissão de entrar** no brinquedo*/
+
+// let permitidos = pessoas.filter((item, index, array)=> {
+//     if(item.idade> 14 && item.idade <60 && item.altura>1.5) {
+//         return item
+//     }
+// })
+// console.log(permitidos)
+
+/*b) Escreva uma função que receba este array e devolva outro array somente com as pessoas que não podem entrar no brinquedo.*/
+
+// let naoPermitidos = pessoas.filter((item, index, array)=> {
+//     if(item.idade < 14 || item.idade > 60 || item.altura < 1.5) {
+//         return item
+//     }
+// })
+
+// console.log(naoPermitidos)
+
+//4 Você foi contratado por um escritório médico para gerar e-mails automáticos para seus clientes, lembrando-os de sua consulta marcada; ou avisa-los que foi cancelada. Considere, então, essas consultas:
+
+// const consultas = [
+// 	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+// 	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+// 	{ nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+// 	{ nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+// ]
+
+// let email = consultas.map((item,index,array)=>{
+//     if(item.cancelada) {
+//         return `Olá, Sr./Sra. ${item.nome}. Infelizmente, sua consulta marcada
+//         para o dia ${item.dataDaConsulta} foi cancelada. Se quiser, pode entrar em 
+//         contato conosco para remarcá-la`
+//     } else {
+//         return  `Olá,  Sr./Sra. ${item.nome} . Estamos enviando esta mensagem para lembrá-lo / lembrá-la
+//         da sua consulta no dia ${item.dataDaConsulta }. Por favor, acuse
+//         o recebimento deste e-mail.`
+//     }
+// })
+
+// console.log(email)
+
+// Preciso encontrar uma forma de usar item.genero
+
+//5 Agora, pediram para você ajudar a fazer uma funcionalidade de um banco digital. Antes de explicar a sua tarefa, você precisa entender como eles guardam as contas dos clientes. Basicamente, eles salvam o nome do clientes, o saldo total e uma lista contendo todas as compras realizadas pelo cliente. Veja abaixo:
+
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+//A sua tarefa é: faça uma função que receba um **array** com os objetos do tipo acima e atualize o **saldo total** individual de cada um, **sem criar um novo** array.
+
+let atualizacao = contas.forEach(element => {
+        return element.saldoTotal - element.compras[i]
+});
+
+console.log(atualizacao)
