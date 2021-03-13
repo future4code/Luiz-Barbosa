51a5558068efb04ca57f22b38eba691fa1a96355
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-03-13 15:59:48 -0300

    hook adicionado

--- quinzena9/quinzena9-aula31/quinzena9-aula31/src/components/areaPrivada/AreaPrivada.js
+++ quinzena9/quinzena9-aula31/quinzena9-aula31/src/components/areaPrivada/AreaPrivada.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {DivPrincipal, BotaoAreaPrivada} from './AreaPrivadaStyles'
 import {useHistory} from 'react-router-dom'
+import { useProtectedPage } from '../../hooks/useProtectedPage'
 
 
 const AreaPrivada = () => {
     const history = useHistory()
 
     const onClickInscritos = () => {
-    history.replace("/inscritos")
+        history.replace("/inscritos")
     }
     const onClickViagens = () => {
-    history.replace("/admViagens")
+        history.replace("/admViagens")
     }
-
-
+    
+    useProtectedPage()
+    
     return(
         <DivPrincipal>
            <BotaoAreaPrivada onClick={onClickInscritos}>Inscritos</BotaoAreaPrivada>
---
+++ quinzena9/quinzena9-aula31/quinzena9-aula31/src/hooks/useProtectedPage.js
@@ -0,0 +1,19 @@
+import { useHistory } from "react-router"
+import {useEffect} from 'react'
+
+
+
+export const useProtectedPage = () => {
+    const history = useHistory()
+
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (!token) {
+            history.replace("/login")
+        }
+    }, [])
+    
+}
+
+
